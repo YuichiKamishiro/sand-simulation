fbebcd50c22ceb8d3b1dfa98ad5b041d6aa8e688
YuichiKamishiro <yuichikamishiro@gmail.com>
2024-07-20 14:37:35 +0300

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,81 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*};
 
-const ROWS: i32 = 10;
-const COLS: i32 = 20;
-const CELL_SIZE: f32 = 32.;
-#[derive(Clone, Copy)]
-struct Cell {
-    x: f32,
-    y: f32,
-}
+const ROWS: i32 = 30;
+const COLS: i32 = 50;
+const CELL_SIZE: f32 = 25.;
 
 fn draw_field() {
     for r in 0..ROWS {
         for c in 0..COLS {
-            draw_rectangle_lines(c as f32 * CELL_SIZE, r as f32 * CELL_SIZE, CELL_SIZE, CELL_SIZE, 1., WHITE);
+            draw_rectangle_lines(
+                c as f32 * CELL_SIZE,
+                r as f32 * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE,
+                1.,
+                WHITE,
+            );
         }
     }
 }
-
-fn draw_sand(cells: &Vec<Cell>) {
-    for cell in cells.iter() {
-        draw_rectangle(cell.x, cell.y, CELL_SIZE, CELL_SIZE, RED);
-    }
-}
-
 #[macroquad::main("Main")]
 async fn main() {
-    let mut cells: Vec<Cell> = vec![];
+    let mut cells = vec![vec![false; COLS as usize]; ROWS as usize];
     let mut time_since_placed: f32 = 0.;
     let mut tick_time: f32 = 0.;
+
     loop {
         clear_background(BLACK);
 
         time_since_placed = time_since_placed + get_frame_time();
         tick_time = tick_time + get_frame_time();
 
-        if is_mouse_button_down(MouseButton::Left) && time_since_placed > 0.050 {
+        if is_mouse_button_down(MouseButton::Left) && time_since_placed > 0.005 {
             time_since_placed = 0.;
             let pos = mouse_position();
-            cells.push(Cell {
-                x: pos.0 - pos.0 % CELL_SIZE,
-                y: pos.1 - pos.1 % CELL_SIZE,
-            });
+            if pos.0 <= COLS as f32 * CELL_SIZE && pos.1 <= ROWS as f32 * CELL_SIZE {
+                cells[(pos.1 / CELL_SIZE) as usize][(pos.0 / CELL_SIZE) as usize] = true;
+            }
         }
 
-        if tick_time > 0.1 {
+        if tick_time > 0.005 {
             tick_time = 0.;
-            for cell in cells.iter_mut() {
-                if cell.y != ((ROWS - 1) * 32) as f32 {
-                    cell.y = cell.y + CELL_SIZE;
+            let copied = cells.clone();
+            for r in 0..ROWS {
+                for c in 0..COLS {
+                    if copied[r as usize][c as usize] == true && r + 1 != ROWS {
+
+                        if cells[(r + 1) as usize][c as usize] == false {
+                            cells[r as usize][c as usize] = false;
+                            cells[(r + 1) as usize][c as usize] = true;
+                        } else if c + 1 != COLS && cells[(r + 1) as usize][(c + 1) as usize] == false {
+                            cells[r as usize][c as usize] = false;
+                            cells[(r + 1) as usize][(c + 1) as usize] = true;
+                        } else if c - 1 != -1 && cells[(r + 1) as usize][(c - 1) as usize] == false {
+                            cells[r as usize][c as usize] = false;
+                            cells[(r + 1) as usize][(c - 1) as usize] = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        for r in 0..ROWS {
+            for c in 0..COLS {
+                if cells[r as usize][c as usize] == true {
+                    draw_rectangle(
+                        c as f32 * CELL_SIZE,
+                        r as f32 * CELL_SIZE,
+                        CELL_SIZE,
+                        CELL_SIZE,
+                        YELLOW
+                    );
                 }
             }
         }
 
         draw_field();
-        draw_sand(&cells);
 
         next_frame().await
     }
